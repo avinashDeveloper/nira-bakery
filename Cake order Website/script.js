dca3deb531f1b499a65cadc77c71127d357e3489
Avinash Kumar <105458143+avinashDeveloper@users.noreply.github.com>
2024-01-23 17:01:05 +0530

    Add files via upload

---
+++ Cake order Website/script.js
@@ -0,0 +1,21 @@
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+function openWhatsApp() {
+    returnwindow.location.href = 'https://wa.me/9102645983'; // Replace with your WhatsApp number
+}
